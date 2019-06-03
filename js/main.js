1a0e7ef248f85a597df147f9bad03cbad613db88
EC2 Default User <ec2-user@ip-172-31-33-30.us-east-2.compute.internal>
2019-06-03 07:34:57 +0000

    hoge

--- js/main.js
+++ js/main.js
@@ -6,8 +6,17 @@
  outLog('run main.js');
  
  
- // ログ出力
- const outLog = (str) => {
-     console.log(str);
-     return;
- }
+// ログ出力
+const outLog = (str) => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const day = now.getDate();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+    const mSecond = now.getMilliseconds();
+
+    console.log(`${year}/${month}/${day} ${hour}:${minute}:${second}.${mSecond} ${str}`);
+    return;
+}
