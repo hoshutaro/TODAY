6d95343d859e41cf267cd72bff312b90a5710352
EC2 Default User <ec2-user@ip-172-31-33-30.us-east-2.compute.internal>
2019-06-03 08:08:47 +0000

    hoge

--- js/main.js
+++ js/main.js
@@ -42,9 +42,49 @@ const importCSS = async () => {
 }
 
 /** ============================================================================
+ * 検索機能
+ * ===========================================================================*/
+
+/**
+ * 検索フォーム設置
+ */
+const addSearchForm = (HEADER) => {
+
+    if(HEADER != null){
+        
+        let cont = document.createElement('div');
+        cont.innerHTML = `<div class="kintoneplugin-input-outer">
+                              <input class="kintoneplugin-input-text" type="text" placeholder="検索文字を入力">
+                          </div>
+                          <button class="kintoneplugin-button-dialog-ok" onClick="doSearch()" style="min-width: 60px;">検索</button>`;
+        
+        HEADER.appendChild(cont);
+    }
+    return;
+}
+
+/** ============================================================================
  * メイン処理
  * ===========================================================================*/
 outLog('run main.js');
+
+/**
+ * レコード一覧画面
+ */
+kintone.events.on('app.record.index.show', (event) => {
+    // 非同期処理を制御
+    appRecordIndexShow();
+
+    return event;
+});
+const appRecordIndexShow = async () => {
+    const HEADER = await kintone.app.getHeaderMenuSpaceElement();
+
+    // 検索フォーム生成
+    await addSearchForm(HEADER);
+    
+    return;
+}
 
 /**
  * 非同期処理を順番に実行させる
